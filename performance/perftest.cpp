7ceee97165087e27d4cf4cbbd741afbb174c26e9
Kjell Hedstrom <kjell.hedstrom@logrhythm.com>
2018-02-14 12:33:31 -0700

    in progress

--- performance/perftest.cpp
+++ performance/perftest.cpp
@@ -9,27 +9,22 @@
 #include <vector>
 #include "RifleVampireProducerConsumer.h"
 #include <cstdio>
+#include <AlarmClock.h>
+
 
 /* Returns microseconds since epoch */
 uint64_t timestamp_now() {
    return std::chrono::high_resolution_clock::now().time_since_epoch() / std::chrono::microseconds(1);
 }
 
-template < typename Function >
-void run_log_benchmark(Function&& f, const std::string& what) {
-   int iterations = 100000;
-   std::vector < uint64_t > latencies;
-   char const* const benchmark = "benchmark";
-   for (int i = 0; i < iterations; ++i) {
-      uint64_t begin = timestamp_now();
-      f(i, benchmark);
-      uint64_t end = timestamp_now();
-      latencies.push_back(end - begin);
-   }
-   std::sort(latencies.begin(), latencies.end());
+const size_t kQSize = 1048576; // 2 ^ 20
+
+
+void PrintResult(const std::string& text, std::vector<uint64_t>&  data) {
    uint64_t sum = 0; for (auto l : latencies) { sum += l; }
-   printf("%s percentile latency numbers in microseconds\n%9s|%9s|%9s|%9s|%9s|%9s|%9s|\n%9ld|%9ld|%9ld|%9ld|%9ld|%9ld|%9lf|\n"
-          , what
+
+   printf("%s\nPercentile latency numbers in microseconds\n%9s|%9s|%9s|%9s|%9s|%9s|%9s|\n%9ld|%9ld|%9ld|%9ld|%9ld|%9ld|%9lf|\n"
+          , text
           , "50th"
           , "75th"
           , "90th"
@@ -47,13 +42,51 @@ void run_log_benchmark(Function&& f, const std::string& what) {
          );
 }
 
+
+
 template < typename Function >
-void run_benchmark(Function&& f, int thread_count, const std::string& what) {
-   printf("\nThread count: %d\n", thread_count);
-   std::vector < std::thread > threads;
-   for (int i = 0; i < thread_count; ++i) {
-      threads.emplace_back(run_log_benchmark<Function>, std::ref(f), what);
+void run_log_benchmark(Function&& f, const std::string& what) {
+   AlarmClock<std::chrono::milliseconds> runTimeCheck(2 * 60 * 1000); // 2 minutes
+   int iterationCount;
+   std::vector < uint64_t > latencies;
+   std::vector < uint64_t > success_latencies;
+   std::vector < uint64_t > failure_latencies;
+   
+   size_t countSuccessful = 0;
+   size_t amount = 0;
+   while(!runTimeCheck.Expired()) {
+      ++amount;
+      uint64_t begin = timestamp_now();
+      success = f(amount);      
+      auto delay = end - begin;
+      uint64_t end = timestamp_now();
+      latencies.push_back(delay);
+      if (success) {
+        success_latencies.push_back(delay);
+      } else {
+        failure_latencies.push_back(delay);        
+      }
    }
+
+   std::sort(latencies.begin(), latencies.end());
+   PrintResult(what + " All Latencies", latencies);
+
+   std::sort(success_latencies.begin(), success_latencies.end());
+   PrintResult(what + " Success Latencies", success_latencies);
+   
+   std::sort(failure_latencies.begin(), failure_latencies.end());
+   PrintResult(what + "Failure Latencies", failure_latencies);
+}
+
+
+
+template < typename Function >
+void run_benchmark(Function&& producer, Function&& consumer, const std::string& whatProducer, const std::string& whatConsumer) {
+   std::vector<std::thread> threads;
+   threads.emplace_back(run_log_benchmark<Function>, std::ref(producer), whatProder);
+   threads.emplace_back(run_log_benchmark<Function>, std::ref(consumer), whatConsumer);
+  
+
    for (int i = 0; i < thread_count; ++i) {
       threads[i].join();
    }
@@ -65,7 +98,56 @@ void print_usage() {
 }
 
 
+
+std::vector<std::string> _received;
+struct Consumer {
+     CircularFifo<void*, kQSize>& _q;
+
+     Producer(CircularFifo<void*, kQSize>& q) : _q(q) {}
+   
+     bool Get() {
+      void* data = nullptr;
+      if (_q.pop(data)) {
+        std::string* extracted = reinterpret_cast<std::string*>(data);
+        _received.push_back({*extracted});
+        delete extracted;
+      return true;
+     }
+     return false;
+   }
+};
+
+
+
+
+std::vector<std::string> _sent;
+struct Producer {
+     CircularFifo<void*, kQSize>& _q;
+     Producer(CircularFifo<void*, kQSize>& q) : _q(q) {}
+   
+     bool Push(int data) {
+      std::string* dataStr = new std::string(std::to_string(data));
+      void* raw = reinterpret_cast<void*>(dataStr);
+
+      if (_q.push(raw)) {
+        _sent.push_back({std::to_string(data)});
+        return true;
+     }
+     delete dataStr;
+     return false;
+   }
+};
+
+
+
+
+void Push()
+
+
+
+
 int main(int argc, char* argv[]) {
+   CircularFifo<void*, kQSize> q;
    if (argc != 2) {
       print_usage();
       return 0;
@@ -73,13 +155,11 @@ int main(int argc, char* argv[]) {
       int thread_count = 1;
 
    if (strcmp(argv[1], "lockfree") == 0) {
-      CircularFifo<std::string*, 1024> q;
-
       auto producer = createProducer(q);
       auto consumer = createConsumer(q);
 
       run_benchmark("lock_free", producer, consumer);
-
+}
 
    // } else if (strcmp(argv[1], "queuenado") == 0) {
    //    auto doneReceiving = std::async(std::launch::async, &Receive);
